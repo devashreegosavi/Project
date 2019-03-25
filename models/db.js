0f6e9c126dab27ec32de24e06ef0fd595ca6752b
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2019-03-25 12:47:42 +0530

    25march19

---
+++ models/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, (err) => {
+    if(!err) { console.log('mongodb connection succeeded') }
+    else { console.log('error in mongodb connection :' + JSON.stringify(err,undefined,2)) }
+})
