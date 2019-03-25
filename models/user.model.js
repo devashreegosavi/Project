5e96b9cccf871d53d27678c274d64a5595ebc1de
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2019-03-25 13:41:06 +0530

    25march

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt = require('bcryptjs')
 var userSchema = new mongoose.Schema({
     fullName : {
         type : String
@@ -13,4 +13,13 @@ var userSchema = new mongoose.Schema({
     saltSecret : String
 })
 
+userSchema.pre('save',function (next){
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(this.password,salt,(err,hash) =>{
+            this.password = hash
+            this.saltSecret = salt
+            next()
+        })
+    })
+})
 mongoose.model('User',userSchema)
