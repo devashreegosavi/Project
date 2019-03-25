8a7803baabdd5833223d52e205b3423d3ca87be4
Devashree Gosavi <35646522+devashreegosavi@users.noreply.github.com>
2019-03-25 13:26:31 +0530

    25 march

--- app.js
+++ app.js
@@ -2,5 +2,13 @@ require('./config/config')
 require('./models/db')
 
 const express = require('express')
-const bodyParser = require('body-psrser')
+const bodyParser = require('body-parser')
 const cors = require('cors')
+
+const rtsIndex = require('./routes/index.router')
+var app = express()
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/api',rtsIndex)
+app.listen(process.env.PORT, () => console.log(`server started at port : ${process.env.PORT}`))
--- models/db.js
+++ models/db.js
@@ -4,3 +4,5 @@ mongoose.connect(process.env.MONGODB_URI, (err) => {
     if(!err) { console.log('mongodb connection succeeded') }
     else { console.log('error in mongodb connection :' + JSON.stringify(err,undefined,2)) }
 })
+
+require('./user.model')
